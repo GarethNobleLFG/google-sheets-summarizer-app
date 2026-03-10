266f59014e796750989c0500ecc698f1a70cffa1
Gareth Noble <garethnoble3@gmail.com>
2026-03-10 19:58:49 -0400

    feat: app router setup

---
+++ frontend/src/pages/main-page/MainPage.tsx
@@ -0,0 +1,8 @@
+export const MainPage = () => {
+    return (
+        <div>
+            <h1>Welcome to Google Sheets Summarizer</h1>
+            <p>This is the main page of your application.</p>
+        </div>
+    );
+};
