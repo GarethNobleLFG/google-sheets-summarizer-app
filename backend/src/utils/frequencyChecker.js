f65340009283944a3875f53599740cda568cd20b
Gareth Noble <garethnoble3@gmail.com>
2026-04-15 17:14:53 -0400

    fix: freq checker now returns false if cron is set to none

--- backend/src/utils/frequencyChecker.js
+++ backend/src/utils/frequencyChecker.js
@@ -1,13 +1,14 @@
-// Check if summary should be executed based on frequency
 export function checkIfShouldExecute(sheetData) {
+    if (sheetData.frequency && sheetData.frequency.toLowerCase() === 'none') {
+        return false;
+    }
+    
     const now = new Date();
     
-    // If we have a next_run_at time, check if it's time to run
     if (sheetData.next_run_at) {
         const nextRunTime = new Date(sheetData.next_run_at);
         return now >= nextRunTime;
     }
     
-    // Default: don't execute if we can't determine schedule
     return false;
 }
