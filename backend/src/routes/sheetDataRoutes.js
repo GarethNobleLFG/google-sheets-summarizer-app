a7fc63e462761d2e3b2c97735fc99856ff82b80c
Gareth Noble <garethnoble3@gmail.com>
2026-04-15 13:54:40 -0400

    feat: enabled authentication layer to sheet data api services

--- backend/src/routes/sheetDataRoutes.js
+++ backend/src/routes/sheetDataRoutes.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import * as sheetDataController from '../controllers/sheetDataControllers.js';
-// import { authenticateToken } from '../middleware/authMiddleware.js';
+import { authenticateToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 // Protected routes.. (authentication required)
-router.post('/', /* authenticateToken, */ sheetDataController.createSheetData);
-router.get('/:id', /* authenticateToken, */ sheetDataController.getSheetDataById);
-router.get('/user/:userId/all', /* authenticateToken, */ sheetDataController.getAllSheetDataFromUser);
-router.put('/:id', /* authenticateToken, */ sheetDataController.updateSheetData);
-router.delete('/:id', /* authenticateToken, */ sheetDataController.deleteSheetData);
-router.delete('/user/:userId/all', /* authenticateToken, */ sheetDataController.deleteAllUserSheetData);
-router.post('/user/:userId/trigger', /* authenticateToken, */ sheetDataController.triggerUserSummaries);
+router.post('/', authenticateToken, sheetDataController.createSheetData);
+router.get('/:id', authenticateToken, sheetDataController.getSheetDataById);
+router.get('/user/:userId/all', authenticateToken, sheetDataController.getAllSheetDataFromUser);
+router.put('/:id', authenticateToken, sheetDataController.updateSheetData);
+router.delete('/:id', authenticateToken, sheetDataController.deleteSheetData);
+router.delete('/user/:userId/all', authenticateToken, sheetDataController.deleteAllUserSheetData);
+router.post('/user/:userId/trigger', authenticateToken, sheetDataController.triggerUserSummaries);
 router.post('/poll/scheduled-summaries', /* SOME KIND OF VERIFICATION LAYER..., */ sheetDataController.pollUsersForScheduledSummaries);
-router.post('/quick-generate/:id', /* authenticateToken , */ sheetDataController.quickGenerateSummary);
+router.post('/quick-generate/:id', authenticateToken, sheetDataController.quickGenerateSummary);
 
 export default router;
